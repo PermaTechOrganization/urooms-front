8c857bc5db3e827e18b6a4ab8fecf5340d5263bf
Lucía A. <u20211a452@upc.edu.pe>
2024-06-07 21:33:59 -0500

    Add: Publication view & modified styles
    Added publication interface and card. Modified styles for web and mobile navbars.
    
    Co-Authored-By: Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>
    Co-Authored-By: Jean Carlos Achamizo <traxefeder@gmail.com>
    Co-Authored-By: Luis Alberto Trujillo Lopez <104176051+NotLT42@users.noreply.github.com>
    Co-Authored-By: MENDOZA BARCO SAUL ENRIQUE <102072719+SaulMendozaB@users.noreply.github.com>
    Co-Authored-By: Farid <89112952+faridhinostroza@users.noreply.github.com>

---
+++ src/components/elements/NavBarLessor.vue
@@ -0,0 +1,110 @@
+<script setup>
+const userPicture = 'https://cdn-icons-png.flaticon.com/512/6596/6596121.png'
+const name = 'Carlos'
+</script>
+
+<template>
+  <div class="navbar-container">
+    <div class="navbar-logo">
+      <router-link to="/">
+        <img class="logo" src="/src/assets/urooms-logo.png" alt="urooms logo" />
+      </router-link>
+    </div>
+    <div class="navbar-links">
+      <router-link to="/home/publications">Publicaciones</router-link>
+      <router-link to="/home/reservations">Reservas</router-link>
+      <router-link to="/register">Solicitudes</router-link>
+    </div>
+    <div class="welcome-user">
+      <p>Hola, {{ name }}</p>
+    </div>
+    <div class="user-menu">
+      <i class="pi pi-bars" style="font-size: 1rem"></i>
+      <img class="user-avatar" :src="userPicture" alt="user" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.navbar-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  width: 100%;
+  height: 60px;
+  background-color: white;
+  border-bottom: 1px solid #f0f0f0;
+}
+
+.navbar-logo {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  max-height: 20px;
+  width: auto;
+}
+
+.navbar-links {
+  display: flex;
+  gap: 20px;
+  transition: all 0.3s ease;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.navbar-links a {
+  text-decoration: none;
+  color: #929292;
+  padding: 6px 14px;
+  border-radius: 30px;
+  transition: all 0.3s ease;
+}
+
+.navbar-links a:hover {
+  color: rgb(85, 85, 85);
+  background-color: #f0f0f0;
+  transition: all 0.3s ease;
+}
+
+.navbar-links .router-link-exact-active {
+  color: black !important;
+  background-color: transparent !important;
+  transition: all 0.3s ease;
+}
+
+.welcome-user {
+  margin-right: 30px;
+  margin-left: auto;
+}
+
+.user-menu {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  border: 1px solid #d4d4d4;
+  padding: 4px 12px;
+  border-radius: 40px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+.user-menu:hover {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease;
+}
+
+.user-avatar {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+}
+
+@media screen and (max-width: 780px) {
+  .navbar-container {
+    display: none;
+  }
+}
+</style>
--- src/components/pages/HomeView.vue
+++ src/components/pages/HomeView.vue
@@ -1,15 +1,36 @@
 <script setup>
-import NavBar from '../elements/NavBar.vue'
+import NavBarStudent from '../elements/NavBarStudent.vue'
+import NavBarStudentMobile from '../elements/NavBarStudentMobile.vue'
+import NavBarLessor from '../elements/NavBarLessor.vue'
+import NavBarLessorMobile from '../elements/NavBarLessorMobile.vue'
+
+var userRole = 'lessor'
 </script>
 
 <template>
   <div class="home-container">
-    <NavBar />
+    <NavBarStudent v-if="userRole === 'student'" />
+    <NavBarLessor v-else />
     <router-view />
+    <NavBarStudentMobile v-if="userRole === 'student'" />
+    <NavBarLessorMobile v-else />
   </div>
 </template>
 
 <style scoped>
+.home-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+
+.router-view-container {
+  flex-grow: 1;
+  width: 100%;
+}
+
 @media screen and (max-width: 1000px) {
 }
 
